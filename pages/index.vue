8cca0d6e0275070c4d0ba99d2aacb661b9680b06
Yuma Endo <yendou8921@gmail.com>
2023-09-22 18:34:33 +0900

    調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <script setup>
 import { useField } from 'vee-validate';
-const { value } = useField("value")
+import * as yup from 'yup';
+
+const { value: name, errorMessage: nameError } = useField(
+  'nameRule',
+  yup.string().required().min(8)
+);
+const { value: email, errorMessage: emailError } = useField(
+  'emailRule',
+  yup.string().required().email()
+);
 
 </script>
 
 <template>
   <h1>バリデーションサンプル</h1>
-  <input type="text" v-model="value">
-  <p>{{ value }}</p>
+  <input type="text" v-model="name">
+  <p>{{ nameError }}</p>
+  <input type="text" v-model="email">
+  <p>{{ emailError }}</p>
 </template>
