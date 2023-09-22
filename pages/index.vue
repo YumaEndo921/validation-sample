ee82418e2186295da4c6f578745fad182febad29
Yuma Endo <yendou8921@gmail.com>
2023-09-22 14:19:04 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useField } from 'vee-validate';
+const { value } = useField("value")
+
+</script>
+
+<template>
+  <h1>バリデーションサンプル</h1>
+  <input type="text" v-model="value">
+  <p>{{ value }}</p>
+</template>
